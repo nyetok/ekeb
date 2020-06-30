19b7868f608fb8681a961425f4877762a9a7f4ad
nyetok <havocy@gmail.com>
2020-06-30 10:05:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.setViewport({ width: 1280, height: 800 })
-  await page.goto('https://www.nytimes.com/')
-  await page.screenshot({ path: 'nytimes.png', fullPage: true })
-  await browser.close()
-})()
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"]
+  });
+  const tab = await browser.newPage();
+  const text = await (await tab.goto("http://example.com/")).text();
+  console.log(text);
+  console.log("done");
+  browser.close();
+}
+
+main();
